4f40c494a3ad8f037ee4fc0eb69d6499830cf707
aky <akiyugata271@gmail.com>
2026-04-25 17:26:27 +0000

    convert_point関数を追加し、画像サイズに基づいて座標を変換する処理を実装

--- src/leader_follower.cpp
+++ src/leader_follower.cpp
@@ -1,10 +1,29 @@
 #include "leader_follower.hpp"
 
+const cv::Size image_size(1280, 960);
+const int max = 20;
+
+cv::Point convert_point(double x, double y)
+{
+    const double min_len = std::min((double)image_size.height, (double)image_size.width) / (2.0 * max);
+    const double cx = image_size.width / 2.0;
+    const double cy = image_size.height / 2.0;
+    return cv::Point(
+        cvRound(cx + x * min_len),
+        cvRound(cy - y * min_len)
+    );
+}
+
 int main()
 {
-    const cv::Size image_size(1280, 960);
-    const cv::Mat img = cv::Mat::zeros(image_size.height, image_size.width, CV_8UC3);
+    const cv::Mat img(image_size.height, image_size.width, CV_8UC3, cv::Scalar(255, 255, 255));
 
+    double x = 0;
+    double y = 0;
+    cv::Point point = convert_point(x, y);
+    const cv::Scalar color_red(0, 0, 255);
+    cv::circle(img, point, 10, color_red, -1);
+    
     cv::imwrite("img/test.png", img);
     return 0;
 }
