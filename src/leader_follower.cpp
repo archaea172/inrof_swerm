337cf1b73628b13b4497a2a61891121129f1edd1
aky <akiyugata271@gmail.com>
2026-04-26 09:22:18 +0000

    followersクラスの最大速度引数を10.0から15.0に変更

--- src/leader_follower.cpp
+++ src/leader_follower.cpp
@@ -48,7 +48,7 @@ void onMouse(int event, int x, int y, int flags, void* userdata)
 int main()
 {
     leader leader_mouse;
-    followers followers_(50, 20, 10.0, 1.1, 0.5, 3, 0.5);
+    followers followers_(50, 20, 15.0, 1.1, 0.5, 3, 0.5);
 
     cv::namedWindow("swerm");
     cv::setMouseCallback("swerm", onMouse, &leader_mouse);
